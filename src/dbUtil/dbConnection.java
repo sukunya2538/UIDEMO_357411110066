ab361fa5f1ebac6116f481b1edeaf9abe9fbb933
sam28092538 <sam28092538@gmail.com>
2018-02-20 12:13:20 +0700

    login

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -5,15 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SQCONN ="jdbc:sqlite:school.sqlite";
-    private static Connection getConnection(){
+    public static final String SQCONN ="jdbc:sqlite:school.sqlite";
+    public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection(SQCONN);
 
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
             e.printStackTrace();
         }
 
